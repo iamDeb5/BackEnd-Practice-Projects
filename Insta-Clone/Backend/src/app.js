7f6b59742051e56d81861047298dd9dc0407568c
Debojyoti <khanradebojyoti@gmail.com>
2026-02-28 02:04:20 +0530

    feat: Initialize Express application with middleware and API route definitions.

--- Insta-Clone/Backend/src/app.js
+++ Insta-Clone/Backend/src/app.js
@@ -26,7 +26,7 @@ app.use("/api/posts", postRouter);
 app.use("/api/users", userRouter);
 
 /* Serve Frontend index.html for any unknown routes */
-app.get("(.*)", (req, res) => {
+app.use((req, res) => {
 	res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
