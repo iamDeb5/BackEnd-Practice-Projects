b37eb5adde529fa1a840ff54484d8fab88f29864
Debojyoti <khanradebojyoti@gmail.com>
2026-04-21 18:09:14 +0530

    feat: initialize frontend project with React, React Router, and Tailwind CSS configuration

--- Snitch/Frontend/src/app/App.jsx
+++ Snitch/Frontend/src/app/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import { RouterProvider } from "react-router";
+import { routes } from "./app.routes.jsx";
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">App</h1>
+      <RouterProvider router={routes} />
     </>
   );
 }
